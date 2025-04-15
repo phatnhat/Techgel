59847e7b1572732cda94645ba560ac6f1fb7b125
phat <nhatphay722@gmail.com>
2025-04-15 08:24:48 +0700

    temp

--- src/main/java/com/techgel/common/entity/adminSettings/WhatWeDoService.java
+++ src/main/java/com/techgel/common/entity/adminSettings/WhatWeDoService.java
@@ -1,2 +1,25 @@
-package com.techgel.common.entity.adminSettings;public class WhatWeDoService {
+package com.techgel.common.entity.adminSettings;
+
+import com.techgel.common.entity.BasedEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "what_we_do_service")
+public class WhatWeDoService extends BasedEntity {
+    private String title_vi;
+    private String title_en;
+    private String image_url;
+    private String image_popup_url;
+
+    @OneToMany(mappedBy = "whatWeDoService", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WhatWeDoServiceItems> whatWeDoServiceItems = new ArrayList<>();
 }
--- src/main/java/com/techgel/common/entity/adminSettings/WhatWeDoServiceItems.java
+++ src/main/java/com/techgel/common/entity/adminSettings/WhatWeDoServiceItems.java
@@ -1,2 +1,24 @@
-package com.techgel.common.entity.adminSettings;public class WhatWeDoServiceItems {
+package com.techgel.common.entity.adminSettings;
+
+import com.techgel.common.entity.BasedEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "what_we_do_service_items")
+public class WhatWeDoServiceItems extends BasedEntity {
+    private String title_vi;
+    private String title_en;
+
+    @ManyToOne
+    @JoinColumn(name = "what_we_do_service_id")
+    private WhatWeDoService whatWeDoService;
 }
