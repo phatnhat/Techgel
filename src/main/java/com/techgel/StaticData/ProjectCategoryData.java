89543ccb7f1e3648ef1c8f463f971ac80e255b2d
Quan Nguyen <48588001+mq003at@users.noreply.github.com>
2025-04-24 23:07:14 +0300

    fix contact, career, images, projects, etc.

--- src/main/java/com/techgel/StaticData/ProjectCategoryData.java
+++ src/main/java/com/techgel/StaticData/ProjectCategoryData.java
@@ -5,14 +5,15 @@ import com.techgel.common.entity.adminSettings.ProjectCategory;
 import java.util.List;
 
 public abstract class ProjectCategoryData {
-    public static List<ProjectCategory> get(){
+    public static List<ProjectCategory> get() {
         ProjectCategory projectCategory1 = new ProjectCategory(1L, "Năng lượng", "Energy", "energy", 1);
         ProjectCategory projectCategory2 = new ProjectCategory(2L, "Công trình nhà ga", "Aviation", "aviation", 2);
         ProjectCategory projectCategory3 = new ProjectCategory(3L, "Công nghiệp", "Industry", "industry", 3);
         ProjectCategory projectCategory4 = new ProjectCategory(4L, "Dân dụng", "Civil", "civil", 4);
-        ProjectCategory projectCategory5 = new ProjectCategory(5L, "Hạ tầng giao thông", "Infrastructure", "infratructure", 5);
+        ProjectCategory projectCategory5 = new ProjectCategory(5L, "Hạ tầng", "Infrastructure", "infrastructure", 5);
         ProjectCategory projectCategory6 = new ProjectCategory(6L, "Công nghệ", "Technology", "technology", 6);
 
-        return List.of(projectCategory1, projectCategory2, projectCategory3, projectCategory4, projectCategory5, projectCategory6);
+        return List.of(projectCategory1, projectCategory2, projectCategory3, projectCategory4, projectCategory5,
+                projectCategory6);
     }
 }
--- src/main/java/com/techgel/common/entity/adminSettings/Carousel.java
+++ src/main/java/com/techgel/common/entity/adminSettings/Carousel.java
@@ -17,12 +17,14 @@ import lombok.Setter;
 public class Carousel extends BasedEntity {
     private int displayOrder;
     private String image_url;
-    private String description;
+    private String description_vi;
+    private String description_en;
 
-    public Carousel(Long id, int displayOrder, String image_url, String description) {
+    public Carousel(Long id, int displayOrder, String image_url, String description_vi, String description_en) {
         super(id);
         this.displayOrder = displayOrder;
         this.image_url = image_url;
-        this.description = description;
+        this.description_vi = description_vi;
+        this.description_en = description_en;
     }
 }
