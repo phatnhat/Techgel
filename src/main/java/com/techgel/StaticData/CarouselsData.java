4f1b13d7c8e3e7381856ac1ce9d67babd797466c
Quan Nguyen <48588001+mq003at@users.noreply.github.com>
2025-04-26 07:44:22 +0300

    fix homepage

--- src/main/java/com/techgel/StaticData/CarouselsData.java
+++ src/main/java/com/techgel/StaticData/CarouselsData.java
@@ -7,21 +7,22 @@ import java.util.List;
 
 public abstract class CarouselsData {
 
-        static String stringOne = "<div><h3>NHÀ GA HÀNH KHÁCH T3 TÂN SƠN NHẤT</h3><span>Khánh thành: Ngày 30/04/2025.</span><br /><span>Dịch vụ: MEP và Quầy làm thủ tục.</span></br /><span>Hình: Hình ảnh thi công kết cấu phần thô.</span></div>";
-        static String stringOneEn = "<div><h3>TAN SON NHAT TERMINAL T3</h3><span>Inauguration: April 30, 2025.</span><br /><span>Services: MEP and Check-in Counters.</span><br /><span>Image: Structural work under construction.</span></div>";
+        static String stringOne = "<div><h3>DỰ ÁN HỆ THỐNG CƠ ĐIỆN.</h3><span class=\"hide-on-mobile\">Khánh thành: 30/04/2025.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Phạm vi công việc: MEP, Quầy làm thủ tục và các dịch vụ khác.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Hình: Hình ảnh trong quá trình thi công.</span></div>";
+        static String stringOneEn = "<div><h3>MEP System Project for Passenger Terminal T3.</h3><span class=\"hide-on-mobile\">Inauguration: 30/04/2025.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Scope of Work: MEP, Check-in Counters, and other services.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Image: Construction progress photos.</span></div>";
 
-        static String stringTwo = "<div><h3>NHÀ GA HÀNH KHÁCH T3 TÂN SƠN NHẤT</h3><span>Khánh thành: Ngày 30/04/2025.</span><br /><span>Dịch vụ: MEP và Quầy làm thủ tục.</span></br /><span>Hình: Nhà ga đón lượt khách đầu tiên.</span></div>";
-        static String stringTwoEn = "<div><h3>TAN SON NHAT TERMINAL T3</h3><span>Inauguration: April 30, 2025.</span><br /><span>Services: MEP and Check-in Counters.</span><br /><span>Image: The terminal welcomes its first passengers.</span></div>";
+        static String stringTwo = "<div><h3>NHÀ GA HÀNH KHÁCH T3 TÂN SƠN NHẤT</h3><span class=\"hide-on-mobile\">Khánh thành: Ngày 30/04/2025.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Dịch vụ: MEP và Quầy làm thủ tục.</span><span class=\"hide-on-mobile\">Hình: Nhà ga đón lượt khách đầu tiên.</span></div>";
+        static String stringTwoEn = "<div><h3>TAN SON NHAT TERMINAL T3</h3><span class=\"hide-on-mobile\">Inauguration: April 30, 2025.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Services: MEP and Check-in Counters.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Image: The terminal welcomes its first passengers.</span></div>";
 
-        static String stringThree = "<div><h3>NHÀ GA HÀNH KHÁCH T3 TÂN SƠN NHẤT</h3><span>Khánh thành: Ngày 30/04/2025.</span><br /><span>Dịch vụ: MEP và Quầy làm thủ tục.</span></br /><span>Hình: Mô hình phác thảo kiến trúc 3D nhà ga.</span></div>";
-        static String stringThreeEn = "<div><h3>TAN SON NHAT TERMINAL T3</h3><span>Inauguration: April 30, 2025.</span><br /><span>Services: MEP and Check-in Counters.</span><br /><span>D architectural visualization of the completed Tan Son Nhat Terminal T3.</span></div>";
+        static String stringThree = "<div><h3>NHÀ GA HÀNH KHÁCH T3 TÂN SƠN NHẤT</h3><span class=\"hide-on-mobile\">Khánh thành: Ngày 30/04/2025.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Dịch vụ: MEP và Quầy làm thủ tục.</span><span class=\"hide-on-mobile\">Hình: Mô hình phác thảo kiến trúc 3D nhà ga.</span></div>";
+        static String stringThreeEn = "<div><h3>TAN SON NHAT TERMINAL T3</h3><span class=\"hide-on-mobile\">Inauguration: April 30, 2025.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Services: MEP and Check-in Counters.</span><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">D architectural visualization of the completed Tan Son Nhat Terminal T3.</span></div>";
+
+        static String stringFour = "<div><h3>SỰ TRỞ LẠI CỦA MỘT TECHGEL HOÀN TOÀN MỚI.</h3><span class=\"hide-on-mobile\">Sau nhiều biến cố, Techgel đang dần hồi phục với sự tham gia của các Cổ đông lớn trong lĩnh vực Hàng Không, Thương Mại, Công Nghiệp, Ngân Hàng và Chứng Khoán.</span class=\"hide-on-mobile\"><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Hình: Ảnh tòa nhà chính công ty.</span></div>";
+        static String stringFourEn = "<div><h3>The Return of a Completely New Techgel.</h3><span class=\"hide-on-mobile\">After many challenges, Techgel is gradually recovering with the participation of major shareholders in Aviation, Commerce, Industry, Banking, and Securities sectors.</span class=\"hide-on-mobile\"><br class=\"hide-on-mobile\"><span class=\"hide-on-mobile\">Image: Photo of the company's main building.</span></div>";
 
         public static List<Carousel> get() {
-                Carousel carousel1 = new Carousel(1L, 1, "/imgs/news/nha-ga-t3-tt/thi-cong-t3.jpg",
-                                StringEscapeUtils.unescapeHtml4(
-                                                "<div><h3>NHÀ GA HÀNH KHÁCH T3 TÂN SƠN NHẤT</h3><span>Khánh thành: Ngày 30/04/2025.</span><br /><span>Phạm vi Công việc: MEP và Quầy làm thủ tục.</span><br /><span>Hình: Nhà ga hành khách trong quá trình thi công.</span></div>"),
-                                StringEscapeUtils.unescapeHtml4(
-                                                "<div><h3>TAN SON NHAT TERMINAL T3</h3><span>Inauguration: April 30, 2025.</span><br /><span>Scope of Work: MEP and Check-in Counters.</span><br /><span>Image: Passenger terminal during construction.</span></div>"));
+                Carousel carousel1 = new Carousel(1L, 2, "/imgs/news/tru-so-techgel/headquarter.webp",
+                                StringEscapeUtils.unescapeHtml4(stringFour),
+                                StringEscapeUtils.unescapeHtml4(stringFourEn));
                 Carousel carousel2 = new Carousel(2L, 2, "/imgs/news/nha-ga-t3-tt/cong-truong-nha-ga.webp",
                                 StringEscapeUtils.unescapeHtml4(stringOne),
                                 StringEscapeUtils.unescapeHtml4(stringOneEn));
